e000bb415fa87546ed1b7db5179b93c424dcd5d8
George Kampanos <george.kampanos@thehutgroup.com>
2018-11-29 23:24:07 +0000

    travis 1

---
+++ src/test/kotlin/service/RetrieveServiceTest.kt
@@ -0,0 +1,16 @@
+package service
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class RetrieveServiceTest {
+
+    @BeforeEach
+    fun setUp() {}
+
+    @Test
+    fun always_fail() {
+        assertTrue(false)
+    }
+}
